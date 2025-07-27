6a1636a730b4a7334b5a5b0bb93fcd25922c6577
R1shabhCodes <rushabhsingh2522@gmail.com>
2025-07-27 18:58:23 +0530

    Added new feature to login/signup

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect
+
+def signup_view(request):
+    from django.contrib.auth.forms import UserCreationForm
+    from django import forms
+    class CustomUserCreationForm(UserCreationForm):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field in self.fields.values():
+                field.help_text = ''
+    message = ''
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            message = 'Please correct the errors below.'
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'pages/signup.html', {'form': form, 'message': message})
+
+def login_view(request):
+    from django.contrib.auth.forms import AuthenticationForm
+    message = ''
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            message = 'Login successful!'
+            return render(request, 'pages/login.html', {'form': form, 'message': message, 'success': True})
+        else:
+            message = 'Invalid username or password.'
+            return render(request, 'pages/login.html', {'form': form, 'message': message, 'success': False})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'pages/login.html', {'form': form, 'message': message})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 from django.shortcuts import render, redirect
 from pages.forms import DonateFoodForm, JoinTeamForm, ContactForm
 
